d3e7bfe0888cc19b951a68d6b5ffbaa2d8af648e
topeisah <110097953+topeisah@users.noreply.github.com>
2023-05-11 04:34:16 +0100

    Add files via upload

---
+++ dojo-blo/src/App.js
@@ -0,0 +1,44 @@
+
+import Navbar from './Navbar';
+import Home from './Home';
+import Create from './Create';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
+function App() {
+  // const title = 'Welcome to the new blog'
+  // const likes = 50
+  // const link = "http://www.google.com"
+  return (
+    <Router>
+    <div className="App">
+      <Navbar/>
+      <div className="content">
+        {/* <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{10}</p>
+        <p>[1,2,3,4,5]</p>
+        <p>{Math.random()* 10}</p> */}
+        {/* <a href={link}>Google Site</a> */}
+        <Switch>
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+          <Route path='/create'>
+            <Create/>
+          </Route>
+          <Route path='/blogs/:id'>
+            <BlogDetails/>
+          </Route>
+          <Route path='*'>
+            <NotFound/>
+          </Route>
+        </Switch>
+      </div>
+    </div>
+    </Router>
+  );
+}
+
+export default App
